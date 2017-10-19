97036b2df4002790922ab082ec859a6d67e4de31
KlausLee <skyfpt1@gmail.com>
2017-10-20 01:36:11 +0900

    nodemailer-ember.js

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,4 +1,28 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  actions : {
+    sendTo(model){
+      //get nodemailer
+      var nodemailer = this.require('nodemailer');
+
+      var transporter = nodemailer.createTransport({
+        service : 'Gmail',  //smtp server
+        auth : {
+          user : 'skyfpt1@gmail.com',
+          pass : 'energetic4936!'
+        }
+      });
+
+      const mailOptions = {
+        from : 'service@9ple.com',
+        to : 'skyfpt@naver.com', //Reciever Email Address
+        subject: 'ttest', // Subject line
+        text: 'test',
+        html: '<p><h1>html contents</h1></p>'
+      };
+
+      transporter.sendMail(mailOptions);
+    }
+  }
 });
